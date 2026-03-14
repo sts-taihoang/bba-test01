b64c68ddbd36a3387cd6c90a2e17f2e722f4bf45
test 01 - tai.hoang <sts.taihoang@gmail.com>
2026-03-14 13:52:40 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,12 @@
+let powerUp = "check";
+if (powerUp === "mushroom"){
+    console.log("Mario becomes Super!");
+} else if (powerUp === "flower") {
+    console.log("Mario can shoot fireballs!");
+} else if (powerUp === "star") {
+    console.log("Mario is invincible!");
+} else if (powerUp === "none") {
+    console.log("Mario is normal!");
+} else {
+    console.log("Unknown power-up")
+}
