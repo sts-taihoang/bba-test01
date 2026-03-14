8b3419337869b7cdfea7a281e7219f9a103a22a0
test 01 - tai.hoang <sts.taihoang@gmail.com>
2026-03-14 13:42:45 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let playerName = "Mario";
+let currentLives = 3;
+
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+
+let tongCoin = level1 + level2 + level3;
+let giaTriTrungBinh = tongCoin % 3;
+
+console.log(giaTriTrungBinh);
