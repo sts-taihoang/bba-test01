32d5e42f2ea1fa665ad43ecd03d95e33f1c5d23a
test 01 - tai.hoang <sts.taihoang@gmail.com>
2026-03-14 15:23:36 +0700

    feat print leaderboard

--- tests/lesson-04/moon-kingdom.js
+++ tests/lesson-04/moon-kingdom.js
@@ -9,10 +9,38 @@ function createCharacters() {
             charLevel: powerUp.charLevel * 2,
             charHealth: powerUp.charHealth * 3
     }))
-    console.log(charactersPowerUp);
+    // console.log(charactersPowerUp);
 
     const possibleWinners = charactersPowerUp.filter(filterChar => filterChar.charHealth > 1000);
     console.log(possibleWinners);
 }
 
+function printLeaderboard(){
+    const players = [
+        {name: "Nah", score: 999},
+        {name: "VD", score: 666},
+        {name: "Acy", score: 83},
+        {name: "Torai9", score: 9},
+        {name: "Tai", score: 2105},
+    ];
+
+    const trueLeaderboard = players.sort((a, b) => b.score - a.score);
+    // console.log(trueLeaderboard);
+
+    for(let i = 0; i < trueLeaderboard.length; i++){
+        let medal = "  ";
+
+        if (i == 0){
+            medal = "🥇";
+        } else if (i == 1){
+            medal = "🥈";
+        } else if (i == 2){
+            medal = "🥉";
+        }
+
+        console.log(medal + " " + (i + 1) + ". " + trueLeaderboard[i].name + " - " + trueLeaderboard[i].score + " pts")
+    }
+}
+
 createCharacters();
+printLeaderboard();
